ee12e3dc5db0979e7643bd55e46985dd1cc9eea6
ecfortes <98495750+ecfortes@users.noreply.github.com>
2024-11-22 16:09:19 -0300

    modified:   controllers/tabletController.js 	modified:   views/tabletZIA.ejs

--- controllers/tabletController.js
+++ controllers/tabletController.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 exports.tabletZIA = async (req, res) => {
   const id_machine = req.params.id;
-  const apiUrl = `http://35.196.181.62:1880/maquinas/${id_machine}`;
+  const apiUrl = `http://192.168.1.16:1880/maquinas/${id_machine}`;
 
   try {
     // Fazer a requisição à API
@@ -11,8 +11,10 @@ exports.tabletZIA = async (req, res) => {
     if (response.status !== 200) {
       throw new Error(`Erro: status ${response.status} da API`);
     }
-
+    //console.log("response.data")
     const data = response.data;
+
+    //onsole.log(data)
 
     // Verifica se 'data.apontamentos' é um array de pares antes de filtrar
     const filteredApontamentos = Array.isArray(data.apontamentos)
@@ -21,12 +23,14 @@ exports.tabletZIA = async (req, res) => {
           .map(([apontamentoId, evento]) => ({ id: apontamentoId, ...evento })) // opcional: adiciona o ID do apontamento no objeto final
       : [];
 
-      console.log(filteredApontamentos)
+          //console.log("response.data")
+      //console.log(filteredApontamentos)
 
     // Renderiza a página com `data` disponível
     res.render("tabletZIA", { id_machine, data, filteredApontamentos });
   } catch (error) {
     console.error("Erro ao buscar dados:", error.message);
     res.status(500).send("Erro ao buscar dados da API");
+    
   }
 };
